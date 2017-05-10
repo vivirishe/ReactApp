245973d68115ac416db0fe497ded30a9f3196179
viviana rishe <vivianacuri@gmail.com>
2017-05-10 14:22:58 -0700

    updated

--- public/app.jsx
+++ public/app.jsx
@@ -1,10 +1,12 @@
 // Pulling the dom into a React component
 // takes one argument and options object
+// only one root (div) to render the view 
 var Greeter = React.createClass({
   render: function() {
     return (
       <div>
         <h1> React! </h1>
+        <p>This is from the component</p>
       </div>
     );
   }
